c3a8d1f4cedec8a870ecd13caa3ebc14ebdb00bc
wyap-echo <58325947+wyap-echo@users.noreply.github.com>
2021-01-14 19:31:46 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -11,7 +11,7 @@
 #
 
 # Uncomment a feed source
-sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
+#sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
-sed -i '$a src-git jerryk https://github.com/jerrykuku/openwrt-package' feeds.conf.defaul
+#sed -i '$a src-git jerryk https://github.com/jerrykuku/openwrt-package' feeds.conf.defaul
