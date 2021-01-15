3908a51ce4ba7a7146b5a22e69bc176c4085af18
wyap-echo <58325947+wyap-echo@users.noreply.github.com>
2021-01-15 13:14:33 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -12,3 +12,7 @@
 
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+
+#更改主机名
+#sed -i 's/hostname='OpenWrt'/hostname='FriendlyWrt'/g' ./package/base-files/files/bin/config_generate
+
