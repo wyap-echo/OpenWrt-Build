aa31a40b445638620179fe04414c250d08521720
wyap-echo <58325947+wyap-echo@users.noreply.github.com>
2021-01-16 14:26:16 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -14,5 +14,5 @@
 sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
 
 #更改主机名
-sed -i 's/hostname='OpenWrt'/hostname='FriendlyWrt'/g' ./package/base-files/files/bin/config_generate
+sed -i 's/hostname='OpenWrt'/hostname='FriendlyWRT'/g' ./package/base-files/files/bin/config_generate
 
