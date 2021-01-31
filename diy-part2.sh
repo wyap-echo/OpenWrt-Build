8ba59a1fd54e38fcb071b748020fefe4e5c7cc71
wyap-echo <58325947+wyap-echo@users.noreply.github.com>
2021-01-31 19:02:22 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -14,7 +14,7 @@
 #sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
 
 #更改主机名
-sed -i 's/set system.@system[-1].hostname='OpenWrt'/set system.@system[-1].hostname='FriendlyWRT'/g' ./package/base-files/files/bin/config_generate
+sed -i 's/OpenWrt/FriendlyWRT/g' package/base-files/files/bin/config_generate
 
 #修改默认主题为argon
 sed -i 's/luci-theme-bootstrap/luci-theme-argon-18.06/g' ./feeds/luci/collections/luci/Makefile
