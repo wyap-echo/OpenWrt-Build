cdc2887ef7c7d01dceab708b0c826cedb55c7003
wyap-echo <58325947+wyap-echo@users.noreply.github.com>
2022-02-15 18:22:08 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -16,3 +16,5 @@ sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 # Add a feed source
 sed -i '$a src-git jerryk https://github.com/jerrykuku/openwrt-package' feeds.conf.default
 sed -i '$a src-git nezha https://github.com/Erope/openwrt_nezha
+sed -i '$a src-git nas https://github.com/linkease/nas-packages.git;master
+sed -i '$a src-git nas_luci https://github.com/linkease/nas-packages-luci.git;main
